3ca1e9247df1af79035514d6fe27205c87437e0b
vivi <me@vivi.fyi>
2020-11-10 22:56:18 +0100

    Add help prompt on ping

--- bot.py
+++ bot.py
@@ -13,6 +13,38 @@ client = discord.AutoShardedClient(
 @client.event
 async def on_message(message: discord.Message):
     if message.channel.type != discord.ChannelType.news:
+        if message.content in {f"<@!{client.user.id}>", f"<@{client.user.id}>"}:
+            app_info = await client.application_info()
+
+            embed = discord.Embed(
+                title="Printer",
+                description="Auto publish bot for Discord.",
+            )
+            embed.add_field(
+                name="Bot Owner",
+                value=f"[{app_info.owner}](https://discord.com/users/{app_info.owner.id})",
+                inline=True,
+            )
+            embed.add_field(
+                name="Invite Link",
+                value=f"https://discord.com/api/oauth2/authorize?client_id={app_info.id}&permissions=76800&scope=bot",
+                inline=True,
+            )
+            embed.add_field(
+                name="Help",
+                value="\n".join(
+                    [
+                        "**How to prevent auto publishing for some channels?**",
+                        "Revoke the 'Manage Messages' permission from this bot in that channel's permission overrides.",
+                        "",
+                        "**How do I prevent some messages from being published?**",
+                        "Don't use this bot.",
+                    ]
+                ),
+            )
+
+            await message.channel.send(embed=embed)
+
         return
 
     perms = message.channel.permissions_for(message.author)
